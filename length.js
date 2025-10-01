33c5455a07715f245d5f4d3bb6373034c795ee09
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-10-01 23:00:59 +0100

    first commit

---
+++ length.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", async()=>{
+
+  const app = document.getElementById("app");
+  const form = document.getElementById("form");
+
+  const lengthInput = document.getElementById("lengthInput");
+  const unitForm = document.getElementById("unitFrom");
+  const unitTo = document.getElementById("unitTo");
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
